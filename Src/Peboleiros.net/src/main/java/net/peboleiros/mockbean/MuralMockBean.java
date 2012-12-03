ac563ea6ee19c3fbd8414e962c19bac23c7faadb
Leandro Bentes <ext-leandro.bentes@nokia.com>
2012-12-03 15:51:27 -0400

    [UI Impl] Implementation of layout.xhtml

---
+++ Src/Peboleiros.net/src/main/java/net/peboleiros/mockbean/MuralMockBean.java
@@ -0,0 +1,82 @@
+package net.peboleiros.mockbean;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import net.peboleiros.mockentitys.MensagemMural;
+
+import org.primefaces.model.chart.CartesianChartModel;
+import org.primefaces.model.chart.ChartSeries;
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Controller;
+
+@Controller("muralMockBean")
+@Scope("session")
+public class MuralMockBean implements Serializable {
+
+	private static final long serialVersionUID = -6642734447578687952L;
+
+	public MuralMockBean(){
+		
+		MensagemMural mensagemUm = new MensagemMural(1, "Cara o Gildo é muito ruim!", "Paulinho","paulo@gmail.com", new GregorianCalendar(2012,GregorianCalendar.DECEMBER,2,15,22,14));
+		MensagemMural mensagemDois = new MensagemMural(2, "queria ver se alguém é melhor que eu!", "Fenomeno","chicao@gmail.com", new GregorianCalendar(2012,GregorianCalendar.DECEMBER,3,9,20,11));
+		MensagemMural mensagemTres = new MensagemMural(3, "visite meu álbum bit.ly/a2sa2", "Pepe","pepe@gmail.com", new GregorianCalendar(2012,GregorianCalendar.NOVEMBER,21,11,22,13));
+		MensagemMural mensagemQuatro = new MensagemMural(4, "muito legal! convidarei meus amigos", "Rapha","r10@gmail.com", new GregorianCalendar(2012,GregorianCalendar.OCTOBER,4,15,22,14));
+		MensagemMural mensagemCinco = new MensagemMural(5, "enviando um abraço pra galera de Mapinguari", "Roger","caxias@hotmail.com", new GregorianCalendar(2012,GregorianCalendar.NOVEMBER,9,15,22,14));
+		MensagemMural mensagemSeis = new MensagemMural(6, "é noixx!", "Joca Rox","joqua@hotmail.com", new GregorianCalendar(2012,GregorianCalendar.DECEMBER,02,15,22,14));
+		MensagemMural mensagemSete = new MensagemMural(7, "vamos vamos!", "Cobra Cega","jonatas@gmail.com", new GregorianCalendar(2012,GregorianCalendar.DECEMBER,02,15,22,14));
+		MensagemMural mensagemOito = new MensagemMural(8, "ei cara, me diz uma coisa...", "John","john.paul@uol.com", new GregorianCalendar(2012,GregorianCalendar.DECEMBER,02,15,22,14));
+		MensagemMural mensagemNove = new MensagemMural(9, "não véio, na real você...", "Gordito","eder@gmail.com", new GregorianCalendar(2012,GregorianCalendar.DECEMBER,02,15,22,14));
+		MensagemMural mensagemDez = new MensagemMural(10, "fala galerinha, vcs estão sendos testados", "Alvaro","alvaro@gmail.com", new GregorianCalendar(2012,GregorianCalendar.DECEMBER,02,15,22,14));
+		
+		recadosMural.add(mensagemUm);
+		recadosMural.add(mensagemDois);
+		recadosMural.add(mensagemTres);
+		recadosMural.add(mensagemQuatro);
+		recadosMural.add(mensagemCinco);
+		recadosMural.add(mensagemSeis);
+		recadosMural.add(mensagemSete);
+		recadosMural.add(mensagemOito);
+		recadosMural.add(mensagemNove);
+		recadosMural.add(mensagemDez);
+		
+		timesResult = new CartesianChartModel();
+		
+		ChartSeries results = new ChartSeries();
+		
+		results.setLabel("Resultado");
+		results.set("Fluminense", 52);
+		results.set("Atlético-MG", 52);
+		results.set("São Paulo", 52);
+		results.set("Corinthians", 52);
+		
+				
+		
+	}
+	
+	private List<MensagemMural> recadosMural = new ArrayList<MensagemMural>(10);
+	private CartesianChartModel timesResult; 
+	
+	
+
+	public CartesianChartModel getTimesResult() {
+		return timesResult;
+	}
+
+	public void setTimesResult(CartesianChartModel timesResult) {
+		this.timesResult = timesResult;
+	}
+
+	public List<MensagemMural> getRecadosMural() {
+		return recadosMural;
+	}
+
+	public void setRecadosMural(List<MensagemMural> recadosMural) {
+		this.recadosMural = recadosMural;
+	}
+	
+	
+
+}
---
+++ Src/Peboleiros.net/src/main/java/net/peboleiros/mockentitys/MensagemMural.java
@@ -0,0 +1,57 @@
+package net.peboleiros.mockentitys;
+
+import java.util.Calendar;
+
+
+public class MensagemMural {
+
+	private Integer idRecado;
+	private String mensagem;
+	private String nomeAutor;
+	private String emailAutor;
+	private Calendar dataHoraPublicacao;
+	
+	public MensagemMural(){}
+	
+	public MensagemMural(Integer idRecado, String mensagem, String nomeAutor, String emailAutor, Calendar dataHora){
+		this.idRecado=idRecado;
+		this.mensagem=mensagem;
+		this.nomeAutor=nomeAutor;
+		this.emailAutor=emailAutor;
+		this.dataHoraPublicacao=dataHora;
+	}
+	
+	public Integer getIdRecado() {
+		return idRecado;
+	}
+	public void setIdRecado(Integer idRecado) {
+		this.idRecado = idRecado;
+	}
+	public String getMensagem() {
+		return mensagem;
+	}
+	public void setMensagem(String mensagem) {
+		this.mensagem = mensagem;
+	}
+	public String getNomeAutor() {
+		return nomeAutor;
+	}
+	public void setNomeAutor(String nomeAutor) {
+		this.nomeAutor = nomeAutor;
+	}
+	public String getEmailAutor() {
+		return emailAutor;
+	}
+	public void setEmailAutor(String emailAutor) {
+		this.emailAutor = emailAutor;
+	}
+	public Calendar getDataHoraPublicacao() {
+		return dataHoraPublicacao;
+	}
+	public void setDataHoraPublicacao(Calendar dataHoraPublicacao) {
+		this.dataHoraPublicacao = dataHoraPublicacao;
+	}
+	
+	
+
+}
