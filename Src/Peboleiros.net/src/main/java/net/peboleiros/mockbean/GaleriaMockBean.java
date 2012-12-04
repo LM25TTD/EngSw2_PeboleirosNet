00c81e1c930f36edae3f298026705b3ece18608c
Leandro Bentes <ext-leandro.bentes@nokia.com>
2012-12-04 09:49:11 -0400

    [UI Impl] Show gallery implemented

--- Src/Peboleiros.net/src/main/java/net/peboleiros/mockbean/GaleriaMockBean.java
+++ Src/Peboleiros.net/src/main/java/net/peboleiros/mockbean/GaleriaMockBean.java
@@ -18,18 +18,32 @@ public class GaleriaMockBean implements Serializable {
 	 */
 	private static final long serialVersionUID = 67835314811932477L;
 	private List<String> images;
+	private List<String> imagesPartida;
+	
 
     @PostConstruct
     public void init() {
-        images = new ArrayList<String>();
+        images = new ArrayList<String>(10);
 
         for(int i=1;i<=10;i++) {
             images.add("foto" + i + ".jpg");
         }
+        
+        imagesPartida = new ArrayList<String>(2);
+        imagesPartida.add("foto1.jpg");
+        imagesPartida.add("foto4.jpg");
+        
+        
     }
 
     public List<String> getImages() {
         return images;
     }
+
+	public List<String> getImagesPartida() {
+		return imagesPartida;
+	}
+    
+    
 
 }
